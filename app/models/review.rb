0de1add1bb1e528a24e3b884870686dc4e6a2907
GAierken <g.aierken@gmail.com>
2019-11-06 10:34:55 -0500

    done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,20 @@
 class Review
+    attr_accessor :viewer, :movie, :rating
+    @@all = []
+
+    def initialize(viewer, movie, rating)
+        @viewer = viewer
+        @movie = movie
+        @rating = rating
+
+        @@all << self
+    end ### initialize
+
+    def self.all
+        @@all
+    end ### all
+
+    
 
 
-end
+end ##class review
--- tools/console.rb
+++ tools/console.rb
@@ -6,10 +6,20 @@ def reload
   load 'config/environment.rb'
 end
 
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+movie_01 = Movie.new("Titanic")
+movie_02 = Movie.new("Joker")
+movie_03 = Movie.new("Coffee Society")
 
+viewer_01 = Viewer.new("Guligena")
+viewer_02 = Viewer.new("Kaishaer")
+viewer_03 = Viewer.new("Conrad")
+
+review_01 = Review.new(viewer_01, movie_01, 5)
+review_02 = Review.new(viewer_02, movie_01, 4)
+review_03 = Review.new(viewer_03, movie_02, 5)
+review_04 = Review.new(viewer_02, movie_02, 5)
+review_05 = Review.new(viewer_03, movie_03, 3)
+review_06 = Review.new(viewer_02, movie_03, 2)
 
 
 
