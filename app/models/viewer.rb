d205b991d6da9aed26e3b6f9abba577a9c706a84
ozubergz <jmin499@gmail.com>
2019-11-06 10:34:56 -0500

    done

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -11,5 +11,33 @@ class Viewer
   def self.all
     @@all
   end
+
+  def reviews
+    Review.all.select do |instance|
+      instance.viewer == self
+    end
+  end
+  
+  def reviewed_movies
+    reviews.map do |instance|
+      instance.movie
+    end
+  end
+
+  def reviewed_movie?(movie)
+    reviewed_movies.include?(movie)
+  end
+
+  def rate_movie(movie, rating)
+    if(!reviewed_movie?(movie))
+      Review.new(self, movie, rating)
+    else
+      self.reviews.each do |instance|
+        if(instance.movie == movie)
+          instance.rating = rating
+        end
+      end
+    end
+  end
   
 end
--- tools/console.rb
+++ tools/console.rb
@@ -10,8 +10,22 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+movie1 = Movie.new("Taxi Driver")
+movie2 = Movie.new("Goodfellas")
+
+viewer1 = Viewer.new("Jim")
+viewer2= Viewer.new("Stan")
+
+review1 = Review.new(viewer1, movie1, 8)
+review2 = Review.new(viewer2, movie2, 7)
 
 
+viewer3 = Viewer.new("bill")
+review3 = Review.new(viewer3, movie1, 10)
+
+viewer1.reviewed_movie?(movie1)
+
+viewer1.rate_movie(movie2, 9)
 
 
 # ===== WARNING! DO NOT EDIT BELOW THIS LINE ===== #
