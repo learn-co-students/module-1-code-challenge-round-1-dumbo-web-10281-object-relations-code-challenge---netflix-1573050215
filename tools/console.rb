615316f7c61cccc9d61c062ac9ab8b0b28f6d3c3
Imade Osifo <toxsik88@gmail.com>
2019-11-06 10:39:03 -0500

    finish code challenge

--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,16 @@ def reload
   load 'config/environment.rb'
 end
 
+m1 = Movie.new("The Untold Epic of Robert Smalls")
+m2 = Movie.new("Snoop Dogg Commentates UK Elections")
+m3 = Movie.new("Ever Cat video ever. The Movie")
+
+v1 = Viewer.new("Chuck")
+v2 = Viewer.new("Todd")
+v3 = Viewer.new("Larry")
+
+r1 = Review.new(v1, m1, 8)
+
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
