c116258bb0d9a5a87357abf018b60f7705748ada
github username <github email address>
2019-11-06 10:37:02 -0500

    done

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -5,11 +5,32 @@ class Viewer
 
   def initialize(username)
     @username = username
-    self.class.all << self
+    @@all << self
   end
 
   def self.all
     @@all
+  end
+
+  def reviews
+    Review.all.select { |review| review.viewer ==  self  }
+  end
+
+  def reviewed_movies
+    reviews.map { |review| review.movie }
+  end 
+
+  def reviewed_movie?(movie)
+    reviewed_movies.include?(movie)
+  end
+
+  def rate_movie(movie, rating)
+    if reviewed_movie?(movie)
+      movie_reviewed = reviews.find {|review| review.movie == movie }
+      movie_reviewed.rating = rating
+    else
+      Review.new(self,movie,rating)
+    end
   end
   
 end
--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,26 @@ def reload
   load 'config/environment.rb'
 end
 
+bob = Viewer.new("Bob")
+rob =  Viewer.new("Rob")
+slob = Viewer.new("Slob")
+
+it = Movie.new("IT")
+donnie_darko = Movie.new("Donnie Darko")
+doctor_sleep = Movie.new("Doctor Sleep")
+
+
+
+r1 = Review.new(bob,it,4)
+r2 = Review.new(bob,donnie_darko,10)
+r3 = Review.new(bob,doctor_sleep,1)
+r4 = Review.new(rob,it,3)
+r5 = Review.new(rob,donnie_darko,10)
+r6 = Review.new(rob,doctor_sleep,10)
+r7 = Review.new(slob,it,1)
+r8 = Review.new(slob,doctor_sleep,1)
+r9 = Review.new(slob,donnie_darko,2)
+
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
