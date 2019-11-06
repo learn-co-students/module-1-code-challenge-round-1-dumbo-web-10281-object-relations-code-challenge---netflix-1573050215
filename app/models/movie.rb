946473036b89d2cf5d5d394f387af7a089d67434
Dominic Chu <dominic.m.chu@gmail.com>
2019-11-06 10:33:47 -0500

    done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,4 +12,51 @@ class Movie
     @@all
   end
 
+  def reviews 
+    Review.all.select do |instance|
+      instance.movie == self
+    end 
+  end 
+
+  def reviewers 
+    reviews.map do |instance|
+      instance.viewer 
+    end 
+  end 
+
+  def average_rating 
+    # reviews.sum { |instance| instance.rating } / reviews.count
+
+    ## integer or float?
+    reviews.sum { |instance| instance.rating } * 1.00 / reviews.count
+
+  end 
+
+  def self.highest_rated    
+    total_rating_hash = {}
+    Review.all.each do |review|
+      if total_rating_hash[review.movie]
+        total_rating_hash[review.movie] += review.rating
+      else
+        total_rating_hash[review.movie] = review.rating
+      end 
+    end 
+    total_counting_hash = {}
+    Review.all.each do |review|
+      if total_counting_hash[review.movie]
+        total_counting_hash[review.movie] += 1
+      else
+        total_counting_hash[review.movie] = 1
+      end 
+    end 
+    total_rating_hash.each do |rating_instance|
+      rating_instance[1] * 1.00 / total_counting_hash[rating_instance[0]]
+    end 
+    total_rating_hash.max_by { |i| i[1] }.first
+
+      
+
+  end 
+
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -10,6 +10,25 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+v1 = Viewer.new("user1")
+v2 = Viewer.new("user2")
+v3 = Viewer.new("user3")
+v4 = Viewer.new("user4")
+
+m1 = Movie.new("movie1")
+m2 = Movie.new("movie2")
+m3 = Movie.new("movie3")
+m4 = Movie.new("movie4")
+
+r1 = Review.new(v1, m1, 1)
+r2 = Review.new(v2, m2, 2)
+r3 = Review.new(v3, m3, 3)
+r4 = Review.new(v4, m4, 4)
+r5 = Review.new(v1, m2, 1)
+r6 = Review.new(v1, m3, 1)
+
+
+
 
 
 
